5109a3cab8fdb66b3bb92295344172a589ffb711
Werneser <mishaaldeny@gmail.com>
2025-12-12 21:41:43 +0300

    Базовое распределение архитектуры на слои

---
+++ lib/data/statechange/AppStateScope.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter5/data/state/app.dart';
+
+
+class AppStateScope extends InheritedNotifier<AppState> {
+  AppStateScope({
+    super.key,
+    required AppState appState,
+    required Widget child,
+  }) : super(notifier: appState, child: child);
+
+  static AppState of(BuildContext context) {
+    final scope =
+    context.dependOnInheritedWidgetOfExactType<AppStateScope>();
+    assert(scope != null, 'AppStateScope not found in context');
+    return scope!.notifier!;
+  }
+}
