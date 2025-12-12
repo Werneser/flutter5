26b88320ff28e92e5efc3e5a7ec4a6925f4c9c46
Werneser <mishaaldeny@gmail.com>
2025-12-13 00:22:06 +0300

    Обновление в Рамках Clean Architecture(UI )

--- lib/ui/screens/status_change_screen.dart
+++ lib/ui/screens/status_change_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import '../../domain/models/user_service.dart';
 
 class StatusChangeScreen extends StatelessWidget {
