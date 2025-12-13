ed6c0f781ff8f2a51329cea975ee5c642746bbda
Werneser <mishaaldeny@gmail.com>
2025-12-13 13:18:30 +0300

    Добавление базы данных для регистрации и авторизации

--- lib/data/datasources/auth_remote_datasource.dart
+++ lib/data/datasources/auth_remote_datasource.dart
@@ -1,34 +1,85 @@
-import 'package:flutter5/domain/models/user.dart';
-
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import '../../domain/models/user.dart';
 
 class AuthRemoteDataSource {
-  final List<User> _users = [];
-  User? _currentUser;
+  final FlutterSecureStorage _storage;
+  static const _keyPrefix = 'user_';
+
+  AuthRemoteDataSource(this._storage);
 
   Future<void> registerUser(String name, String login, String password) async {
+    final userId = DateTime.now().millisecondsSinceEpoch.toString();
     final user = User(
-      id: DateTime.now().millisecondsSinceEpoch.toString(),
+      id: userId,
       name: name,
       login: login,
       password: password,
     );
-    _users.add(user);
+
+    await _storage.write(key: '${_keyPrefix}${user.id}_id', value: user.id);
+    await _storage.write(key: '${_keyPrefix}${user.id}_name', value: user.name);
+    await _storage.write(key: '${_keyPrefix}${user.id}_login', value: user.login);
+    await _storage.write(key: '${_keyPrefix}${user.id}_password', value: user.password);
   }
 
-  bool loginUser(String login, String password) {
-    final user = _users.firstWhere(
-          (user) => user.login == login && user.password == password,
-      orElse: () => User(id: '', name: '', login: '', password: ''),
-    );
-    if (user.id.isNotEmpty) {
-      _currentUser = user;
-      return true;
+  Future<bool> loginUser(String login, String password) async {
+    final allUsers = await _getAllUsers();
+    for (var user in allUsers) {
+      if (user.login == login && user.password == password) {
+        await _storage.write(key: 'current_user_id', value: user.id);
+        return true;
+      }
     }
     return false;
   }
 
-  void logout() {
-    _currentUser = null;
+  Future<List<User>> _getAllUsers() async {
+    final allKeys = await _storage.readAll();
+    final users = <User>[];
+    final userIds = <String>{};
+
+    for (var key in allKeys.keys) {
+      if (key.startsWith(_keyPrefix) && key.endsWith('_id')) {
+        final userId = allKeys[key];
+        userIds.add(userId!);
+      }
+    }
+
+    for (var userId in userIds) {
+      final user = await getUserById(userId);
+      if (user != null) {
+        users.add(user);
+      }
+    }
+
+    return users;
+  }
+
+  Future<User?> getUserById(String userId) async {
+    final id = await _storage.read(key: '${_keyPrefix}${userId}_id');
+    final name = await _storage.read(key: '${_keyPrefix}${userId}_name');
+    final login = await _storage.read(key: '${_keyPrefix}${userId}_login');
+    final password = await _storage.read(key: '${_keyPrefix}${userId}_password');
+
+    if (id != null && name != null && login != null && password != null) {
+      return User(
+        id: id,
+        name: name,
+        login: login,
+        password: password,
+      );
+    }
+    return null;
+  }
+
+  Future<User?> getCurrentUser() async {
+    final currentUserId = await _storage.read(key: 'current_user_id');
+    if (currentUserId == null) return null;
+    return await getUserById(currentUserId);
+  }
+
+  Future<void> logout() async {
+    await _storage.delete(key: 'current_user_id');
   }
 }
 
